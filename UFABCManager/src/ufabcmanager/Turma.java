f368cc511d19cbc1daa20a5b217cd36f7f6236af
brunoburga <bb_borghese@hotmail.com>
2017-11-07 20:02:44 -0200

    Fixes

---
+++ UFABCManager/src/ufabcmanager/Turma.java
@@ -0,0 +1,16 @@
+package ufabcmanager;
+
+import jade.core.Agent;
+
+public class Turma extends Agent {
+    @Override
+    public void setup() 
+    {
+        
+    }
+    
+    protected void takeDown()
+    {
+        
+    }
+}
